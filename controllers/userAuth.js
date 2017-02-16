af571931b4be0067567cf3550f844d4a7ec7d32b
Justin B <bonnerj@purdue.edu>
2017-02-15 21:18:08 -0500

    Add user authentication

---
+++ controllers/userAuth.js
@@ -0,0 +1,24 @@
+/**
+ *  Module dependencies
+ */
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+
+/**
+ *  Validate User
+ */
+passport.use(
+  new LocalStrategy(
+    function(username, password, done){
+      User.findOne({username: username},
+        function(err, user){
+          if(err) return done(err);
+          if(!user) return done(null, false);
+          if(!user.validPassword(password)) return done(null, false);
+          return done(null, user);
+        }
+      );
+    }
+  )
+);
