7acd3e675e697fc69cb9a3f17892e3f716b0580b
Dan Zheng <zheng321@purdue.edu>
2017-02-17 10:36:56 -0500

    Add webpack, sass, bootstrap, font-awesome, jquery
     - Add configuration file: webpack.config.js
     - Add webpack entry file: public/index.js
     - Add node-sass-middleware
     - Add Bootstrap 3, Font-Awesome, and jQuery

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 const dotenv = require('dotenv');
 const express = require('express');
 const path = require('path');
+const sass = require('node-sass-middleware');
 
 /**
  * Load environment variables from .env file
@@ -28,6 +29,11 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.use(sass({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public')
+}));
+app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
 
 /*
  * Define app routes
@@ -53,7 +59,7 @@ models.sequelize.sync().then(() => {
     console.log('Found user: ' + user.emailFullName);
     models.Group.findOrCreate({
       where: {
-        name: 'CS252 Students',
+        name: 'CS 252 Students',
         groupType: 'group',
         description: 'A study group for CS 252 students'
       }
