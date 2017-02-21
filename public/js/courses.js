e5ed4edc31fa8c8708751b156b07a1d3b2e7b931
Dan Zheng <zheng321@purdue.edu>
2017-02-21 15:38:36 -0500

    Update course search autocomplete
     - Add 'search' and 'close' icons
     - Add event listeners for autocomplete:updated, autocomplete:selected

--- public/js/courses.js
+++ public/js/courses.js
@@ -1,12 +1,20 @@
 // Initialize autocomplete menu
-const client = algoliasearch("8NC5DAIBAS", "90e970160f1e67e1ee19fe02af4ea530");
+const client = algoliasearch('8NC5DAIBAS', '90e970160f1e67e1ee19fe02af4ea530');
 const index = client.initIndex('subjects');
+
+// DOM Binding
+const searchInput = $('#aa-search-input');
+const inputContainer = $('#aa-input-container');
+
 // Initialize autocomplete on search input (ID selector must match)
-$('#aa-search-input').autocomplete({ hint: false }, [{
+searchInput.autocomplete({
+  autoselect: true,
+  hint: false
+}, [{
   source: $.fn.autocomplete.sources.hits(index, { hitsPerPage: 5 }),
-  // value to be displayed in input control after user's suggestion selection
+  // Value to be displayed in input control after user's suggestion selection
   displayKey: 'name',
-  // hash of templates used when rendering dataset
+  // Hash of templates used when rendering dataset
   templates: {
     // 'suggestion' templating function used to render a single suggestion
     suggestion(suggestion) {
@@ -14,3 +22,28 @@ $('#aa-search-input').autocomplete({ hint: false }, [{
     }
   }
 }]);
+
+// Event handler:
+// When autocomplete is updated, find length of its value and add/remove class 'input-has-value'.
+// This changes the icon rendered in the input.
+searchInput.on('autocomplete:updated', () => {
+  if (searchInput.val().length > 0) {
+    inputContainer.addClass('input-has-value');
+  } else {
+    inputContainer.removeClass('input-has-value');
+  }
+});
+
+// Event handler:
+// When autocomplete is selected, set the value of searchInput to the suggestion.
+searchInput.on('autocomplete:selected', (event, suggestion, dataset) => {
+  searchInput.autocomplete('val', suggestion.Name);
+});
+
+// Event handler:
+// When #icon-close is clicked, clear searchInput and remove class 'input-has-value'.
+// This changes the icon from 'close' to 'search'.
+$('#icon-close').on('click', () => {
+  searchInput.val('');
+  inputContainer.removeClass('input-has-value');
+});
