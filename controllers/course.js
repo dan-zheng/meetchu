ab08dd4e9568e9b4b8f9a1c7decdb40e85f3a60b
Dan Zheng <zheng321@purdue.edu>
2017-02-27 11:51:10 -0500

    Fix flash, add user sync

--- controllers/course.js
+++ controllers/course.js
@@ -1,3 +1,5 @@
+const async = require('async');
+const request = require('superagent');
 const models = require('../models');
 
 /**
@@ -51,4 +53,58 @@ exports.postRemoveCourse = (req, res) => {
     req.user.removeCourse(course);
     return res.redirect('/courses');
   });
+};
+
+/**
+ * POST /courses/auth
+ * Authenticate with Purdue and add courses.
+ */
+exports.postAuthCourses = (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const encodedString = Buffer.from(`${username}:${password}`).toString('base64');
+
+  request
+    .get('https://api-dev.purdue.io/Student/Schedule')
+    .auth(username, password)
+    .then((res2) => {
+      const term = 'spring 2017';
+      const courses = res2.body[term];
+      console.log(courses);
+      async.each(courses, ((courseID, callback2) => {
+        request
+          .get('https://api.purdue.io/odata/Sections')
+          .query({
+            $filter: `SectionId eq ${courseID}`,
+            $expand: 'Class($expand=Course)'
+          })
+          .then((res3) => {
+            const section = res3.body.value[0];
+            const _class = section.Class;
+            const course = _class.Course;
+            models.Course.findOne({
+              where: {
+                title: course.Title
+              }
+            }).then((_course) => {
+              req.user.addCourse(_course);
+              callback2(null);
+            }).catch((err) => {
+              callback2(err);
+            });
+          })
+          .catch((err) => {
+            callback2(err);
+          });
+      }), ((err) => {
+        // If error
+        if (err) {
+          return next(err);
+        }
+        // If success
+        req.session.save(() => {
+          return res.redirect('/courses');
+        });
+      }));
+    });
 };
