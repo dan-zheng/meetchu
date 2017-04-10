017881cff530345012d338daac2923e26bea0545
Dan Zheng <zheng321@purdue.edu>
2017-04-10 16:17:03 -0400

    Add postinstall for installing client dependencies

--- client/start.js
+++ client/start.js
@@ -1,3 +1,5 @@
-const args = [ 'run', 'dev' ];
+const args = process.env.NODE_ENV === 'development' ?
+  [ 'run', 'dev' ] :
+  [ 'run', 'build' ];
 const opts = { stdio: 'inherit', cwd: 'client', shell: true };
 require('child_process').spawn('npm', args, opts);
