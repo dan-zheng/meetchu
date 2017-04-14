7b43932f875cb777b36fd1b6b30d78f7b9b63dc2
Justin B <bonnerj@purdue.edu>
2017-04-14 13:17:02 -0400

    Update meeting model

--- server/models/meeting.js
+++ server/models/meeting.js
@@ -1,4 +1,30 @@
-module.exports = (sequelize, Sequelize) => {
+
+module.exports = {
+  query: [
+    `CREATE TABLE IF NOT EXISTS meeting (
+      id INT NOT NULL AUTO_INCREMENT,
+      name VARCHAR(255) NOT NULL,
+      location VARCHAR(255),
+      description VARCHAR(255),
+      last_updated DATETIME DEFAULT CURRENT_TIMESTAMP,
+      final_time_id INT,
+      PRIMARY KEY (id),
+      FOREIGN KEY (final_time_id)
+        REFERENCES meeting_time(id)
+        ON DELETE CASCADE
+    )`,
+    `CREATE TABLE IF NOT EXISTS meeting_time (
+      id INT NOT NULL AUTO_INCREMENT,
+      time DATETIME NON NULL,
+      meeting_id INT NON NULL,
+      PRIMARY KEY (id)
+      FOREIGN KEY (meeting_id)
+        REFERENCES meeting(id)
+    )`,
+  ]
+};
+
+/*module.exports = (sequelize, Sequelize) => {
   const Meeting = sequelize.define('Meeting', {
     id: {
       type: Sequelize.INTEGER,
@@ -27,4 +53,4 @@ module.exports = (sequelize, Sequelize) => {
     }
   });
   return Meeting;
-};
+};*/
