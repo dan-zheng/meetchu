99ce4942edfdd18c4920a5802c6c58cb623d6b9c
EDawg878 <EDawg878@gmail.com>
2017-04-19 13:06:21 -0400

    Work on course dao

--- server/dao/course.js
+++ server/dao/course.js
@@ -5,8 +5,29 @@ const Maybe = monet.Maybe;
 const Either = monet.Either;
 
 module.exports = models => ({
-  findByUser(user) {
-    return null;
+  findByPerson(user) {
+    return models.pool.query(
+      `SELECT * FROM course
+        JOIN person_course
+        ON person_id = id
+        WHERE person_id = ?`, [user.id])
+      .then(result => Either.Right(result.list()))
+      .errorToLeft();
+  },
+  addPerson(course, person) {
+    return models.pool.query(
+      `INSERT INTO person_course
+        (person_id, course_id)
+        VALUES (?, ?)`, [person.id, course.id])
+      .then(result => Either.Right(result.affectedRows))
+      .errorToLeft();
+  },
+  removePerson(course, person) {
+    return models.pool.query(
+      `DELETE FROM person_course
+        WHERE person_id = ? AND course_id = ?`, [person.id, course.id])
+      .then(result => Either.Right(result.affectedRows))
+      .errorToLeft();
   },
   findByCourseId(id) {
     return models.pool.query('SELECT * FROM course WHERE id = ?', [id])
