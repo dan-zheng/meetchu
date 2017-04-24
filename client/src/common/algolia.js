d2af50a6caad95325b168ce399868af1c0c02b49
EDawg878 <EDawg878@gmail.com>
2017-04-23 20:49:52 -0400

    Add algolia to the frontend for searching usage

---
+++ client/src/common/algolia.js
@@ -0,0 +1,10 @@
+import algoliasearch from 'algoliasearch';
+
+const client = algoliasearch(process.env.ALGOLIA_ID, process.env.ALGOLIA_ADMIN_KEY, { protocol: 'https:' });
+const courseIndex = client.initIndex('courses');
+const userIndex = client.initIndex('users');
+
+export {
+  courseIndex,
+  userIndex
+}
